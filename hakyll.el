2dcf93f6fa89ecd389c7901699fb26b670a78881
Rob Nugen <rob@robnugen.com>
2014-10-30 12:27:56 +0900

    Interactively ask for year, month, date and use those in file path

--- hakyll.el
+++ hakyll.el
@@ -1,25 +1,32 @@
 ;;; hakyll.el --- Convenience functions for working with Hakyll.
 ;;
-;;; Commentary:
+;;; Commentary:      
 ;; Convenience functions for working with Hakyll.
 
 ;;; Code:
 
-(defvar hakyll-site-location "~/Sites/hblog/"
+(defvar hakyll-site-location "~/journal/"
   "The location of the Hakyll files.")
 
-(defun hakyll-new-post (title tags)
+(defun hakyll-new-post (title tags yyyy mm dd)
   "Create a new Hakyll post for today with TITLE and TAGS."
-  (interactive "sTitle: \nsTags: ")
-  (let ((file-name (hakyll-post-title title)))
-    (set-buffer (get-buffer-create file-name))
+  (interactive "sTitle: \nsTags: \nsYear: \nsMonth: \nsDay: ")
+  (let ((file-name (hakyll-post-title title))
+	(file-path (hakyll-post-path title yyyy mm dd)))
+    (set-buffer (get-buffer-create file-path))
     (insert
-     (format "---\ntitle: %s\ntags: %s\ndescription: \n---\n\n"
+     (format "---\ntitle: %s\ntags: %s\ndescription: \nauthor: Rob Nugen\ndate: %s-%s-%s\n---\n\n%s\n"
              title
-             (downcase tags)))
+	     (downcase tags)
+	     yyyy
+	     mm
+	     dd
+	     (format-time-string "##%H:%M %A %d %B %Y %Z##")
+        ))
     (write-file
      (expand-file-name file-name (concat hakyll-site-location "posts")))
-    (switch-to-buffer file-name)))
+    (switch-to-buffer file-name)
+    ))
 
 (defun hakyll-new-note (title)
   "Create a new Note with TITLE."
@@ -34,8 +41,13 @@
 (defun hakyll-post-title (title)
   "Return a file name based on TITLE for the post."
   (concat
-   (format-time-string "%Y-%m-%d")
-   "-"
+   (replace-regexp-in-string " " "-" (downcase title))
+   ".markdown"))
+
+(defun hakyll-post-path (title yyyy mm dd)
+  "Return a file path based on TITLE and date."
+  (concat
+   yyyy "/" mm "/" dd "/"
    (replace-regexp-in-string " " "-" (downcase title))
    ".markdown"))
 

50dc80e485b2de39ec5808abd4e05dc98e123fb4
Rob Nugen <rob@robnugen.com>
2021-08-05 16:54:26 +0900

    Point to blog template

--- hugo.el
+++ hugo.el
@@ -221,7 +221,7 @@
     (set-buffer (get-buffer-create file-path))
     (insert
      (format
-             (get-string-from-file (expand-file-name "journal_template.txt" location-journal-template-files))
+             (get-string-from-file (expand-file-name "blog_template.txt" location-journal-template-files))
              title
              (mapconcat (lambda (x) (format "\"%s\"" (downcase x))) tags ", ")
              yyyy
@@ -231,7 +231,7 @@
              (format-time-string "%H:%M %A %d %B %Y %Z")
              ))
     (write-file
-     (expand-file-name file-path (concat journal-site-location "")))
+     (expand-file-name file-path (concat blog-site-location "")))
     (switch-to-buffer file-name)
     (auto-fill-mode)
   )
